b3adc5ba7039cbdf2ab2a8fb1db05ca8ec13b6f7
MatheusAndrade1995 <114603315+MatheusAndrade1995@users.noreply.github.com>
2022-11-19 02:50:17 -0300

    Update - tempo de partida

--- Aula 4/script.js
+++ Aula 4/script.js
@@ -1,8 +1,9 @@
+
 var jogador1 = 0;
 var jogador2 = 0;
 var placar = 0;
 
-
+//Contagem de registros de pontos por jogador 
 if (jogador1 > 0) {
     console.log("jogador1 " + " marcou ponto! " + jogador1);
     placar = jogador1 > jogador2
@@ -17,6 +18,7 @@ else {
     console.log("Ninguém marcou ponto!");
 }
 
+//Resultado ao fim da partida setando 3 condições para cada possibilidade de placar
 switch (placar) {
     case placar = jogador1 > jogador2:
         console.log('Jogador1 ganhou a partida!');
@@ -27,4 +29,15 @@ switch (placar) {
     case placar = jogador1 = jogador2:
         console.log('Empate!')
         break;
+}
+
+
+//Contagem de minutos da partida
+var minutos = 0;
+
+do {
+    minutos++;
+    console.log("Tempo de partida: " + minutos )
+}while (minutos < 5){
+    console.log("Tempo de partida: " + minutos + " Fim de jogo! ")
 }
