6aade337a2525881595d420b7fc1e5dfa21b1743
MatheusAndrade1995 <114603315+MatheusAndrade1995@users.noreply.github.com>
2022-11-21 19:17:54 -0300

    Add files via upload

---
+++ Atividade JS/ValidarNumeros.js
@@ -0,0 +1,39 @@
+function montaFrase(num1, num2){
+    if (!num1 || !num2) return "Os números inseridos não são validos";
+    const primeiraFrase = criaPrimeiraFrase(num1, num2);
+    const segundaFrase = criaSegundaFrase(num1, num2);
+    
+    return 
+}
+
+function criaPrimeiraFrase(num1, num2){
+    let saoIguais = '';
+
+    if (num1 !== num2){
+        saoIguais = 'Não';
+    }
+
+    return `Os números ${num1} e ${num2} ${saoIguais} são iguais. `;
+}
+
+function criaSegundaFrase(num1, num2){
+    const soma = num1 + num2;
+
+    let resultado10 = 'menor';
+    let resultado20 = 'menor';
+
+    const compara10 = soma > 10;
+    const compara20 = soma > 20;
+
+    if (compara10) {
+        resultado10 = 'maior';
+    }
+
+    if (compara20) {
+        resultado20 = 'maior';
+    }
+
+    return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`;
+}
+
+console.log(montaFrase(2, 20));
---
+++ Aula 5 Funções/script.js
@@ -0,0 +1,23 @@
+function soma (a, b) {
+    a = 5;
+    b = 3;
+
+    adicao = a + b;
+    console.log(adicao);
+}
+soma();
+
+//Expressão de função
+var mensagem = function (primeira, segunda) {
+    console.log(primeira, segunda);
+}
+mensagem('Tipos de', 'funções');
+
+//Arrow Function
+var arrow = () => {
+    console.log('Sou uma Arrow function');
+}
+arrow();
+
+
+
