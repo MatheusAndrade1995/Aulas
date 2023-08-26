605a6b956f51247ab029e4950ce19dcd73bad87d
terceiro-matandrade <terceiro-matandrade@icatuseguro.com.br>
2023-08-26 13:59:15 -0300

    Implantando chamada de formulário

--- Tela-PortaldeLogin/js/script.js
+++ Tela-PortaldeLogin/js/script.js
@@ -9,20 +9,54 @@ document.getElementById('entrar').addEventListener('click', moveOverlay)
 document.getElementById('open-register-mobile').addEventListener('click', moveOverlay)
 document.getElementById('open-login-mobile').addEventListener('click', moveOverlay)
 
+document.getElementById('login-form').addEventListener('submit', cadastro)
 
-function logar(){
-    //abre em outra guia
-    window.open("");
-    //abre na mesma guia
-    window.location.href = "";
+
+function logar(event) {
+    var inputs = document.getElementsByClassName('form-input')
+    for (let input of inputs) {
+        if (input.value == "") {
+            input.parentElement.classList.add("wrap-invalid-input")
+        }else{
+            input.parentElement.classList.remove("wrap-invalid-input")
+        }
+        
+        /* else {
+            //abre em outra guia
+            window.open("");
+            //abre na mesma guia
+            window.location.href = ""
+        }*/
+    }
+    event.preventDefault()
 }
 
-function cadastro(){
-    //abre em outra guia
-    window.open("");
-    //abre na mesma guia
-    window.location.href = "";
+function cadastro(event) {
+    var inputs = document.getElementsByClassName('form-input')
+    for (var input of inputs) {
+        if (input.value.trim() == "") {
+            input.parentElement.classList.add("wrap-invalid-input")
+        }/* else {
+            //abre em outra guia
+            window.open("");
+            //abre na mesma guia
+            window.location.href = ""
+        }*/
+    }
+    event.preventDefault()
+
 }
+/*
+function invalidInput(event){
+    let inputs = document.getElementsByClassName('form-input')
+    for (let input of inputs){
+        if (input.value.trim() == ""){
+            input.parentElement.classList.add("wrap-input-invalid")            
+        }
+    }
+    event.preventDefault()
+}
+
 
 /*function trocar(){
     document.getElementById("cadastrar").addEventListener(login-container.toogle);
