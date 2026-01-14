46fe3dc9d00b21aee46cc2f043be85b5db1861ce
MatheusAndrade1995 <114603315+MatheusAndrade1995@users.noreply.github.com>
2026-01-13 23:34:57 -0300

    Add files via upload
    Neste Lab você será responsável por modelar um sistema que trabalha com celulares. Para isso, foi solicitado que você faça uma abstração de um celular e disponibilize maneiras de diferentes marcas e modelos terem seu próprio comportamento, possibilitando um maior reuso de código e usando a orientação a objetos

---
+++ SmartphoneMda/Program.cs
@@ -0,0 +1,16 @@
+﻿using SmartphoneMda.Models;
+
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia("123456", "AUIJ8535", "16541354684", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+
+Console.WriteLine("/n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone16 = new Iphone("123789", "AAARE8535", "16541355547", 128);
+iphone16.Ligar();
+iphone16.ReceberLigacao();
+iphone16.InstalarAplicativo("CandyCrush");
